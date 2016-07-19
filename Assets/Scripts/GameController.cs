d4653ef0c5a3e7b33771249536906cd8736e91e7
Łukasz Lalik <lukasz.lalik@gmail.com>
2016-07-19 06:41:32 +0200

    Game Over and Game Restart

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,14 +12,34 @@ public class GameController : MonoBehaviour
 	public float waveWait;
 
 	public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
 	private int score;
 
 	void Start()
 	{
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
 		score = 0;
 		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
 	}
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
 
 	IEnumerator SpawnWaves()
 	{	
@@ -37,6 +57,13 @@ public class GameController : MonoBehaviour
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' to restart";
+                restart = true;
+                break;
+            }
 		}
 	}
 
@@ -50,4 +77,10 @@ public class GameController : MonoBehaviour
 	{
 		scoreText.text = "Score: " + score;
 	}
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over";
+        gameOver = true;
+    }
 }
